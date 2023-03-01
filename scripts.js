51f45d23a08095ec09757b59724c55df73006a1a
Mike Costa <mcosta1@gmail.com>
2023-02-28 19:43:06 -0500

    Add the operator functions
    The following functions are added:
    
      add
      subtract
      multiply
      divide

--- scripts.js
+++ scripts.js
@@ -4,3 +4,23 @@ let isDisplayError = false;
 let operatorSelected = null;
 let displayValue = 0;
 let storedValue = 0;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if(b === 0) {
+    return null;
+  }
+  
+  return a / b;
+}
