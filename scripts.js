94b6e662ba29a2056d92b156f8974b168e6e9512
Mike Costa <mcosta1@gmail.com>
2023-02-28 20:24:37 -0500

    Add addDigitToDisplay function
    This will take a single digit of a number (decimals and negative signs
    are not included here) and append it to the display value of the
    calculator.
    
    If the number of digits are of max size, then nothing is added.

--- scripts.js
+++ scripts.js
@@ -21,13 +21,35 @@ function divide(a, b) {
   if(b === 0) {
     return null;
   }
-  
+
   return a / b;
 }
 
 function getCountOfDisplayDigits() {
   const display = document.getElementById('result-value');
   const displayValue = display.textContent;
-  
+
   return displayValue.replace(/[^0-9]/g, '').length;
+}
+
+function addDigitToDisplay(digit) {
+  if(isNaN(digit)) {
+    return;
+  }
+
+  digit = Number(digit);
+
+  if(Math.floor(digit) !== digit || digit < 0 || digit > 9) {
+    return
+  }
+
+  const display = document.getElementById('result-value');
+
+  if(newNumberEntered && getCountOfDisplayDigits() < MAX_DIGITS) {
+    display.textContent += digit.toString();
+
+  } else if(!newNumberEntered) {
+    display.textContent = digit.toString();
+    newNumberEntered = true;
+  }
 }
