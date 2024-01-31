287b110c195b5d296d85a673cb10eb55338d630c
Mike Costa <mcosta1@gmail.com>
2024-01-30 21:04:59 -0500

    Add calculator.js file with skeleton class
    The class, properties, and methods have been defined.  All of the
    methods are currently empty, they will be defined later.

---
+++ scripts/calculator.js
@@ -0,0 +1,58 @@
+class Calculator {
+
+  constructor() {
+    this.resetCalculator();
+  }
+
+  static MAX_DIGITS = 10;
+  static MAX_VALUE = Math.pow(10, MAX_DIGITS) - 1;
+  static MIN_VALUE = MAX_VALUE * -1;
+
+  static getCountOfNumericalDigits(numberString) {
+
+  }
+
+  static isStringANumber(numberString) {
+
+  }
+
+  resetCalculator() {
+
+  }
+
+  massInput(inputSequenceString) {
+
+  }
+
+  input(char) {
+
+  }
+
+  inputNegativeSign() {
+
+  }
+
+  inputDigit(digitChar) {
+
+  }
+
+  inputEquals() {
+
+  }
+
+  inputOperator(nextOperator) {
+
+  }
+
+  executeOperation() {
+
+  }
+
+  deleteExtraDecimalDigits() {
+
+  }
+
+  deleteLastDigit() {
+
+  }
+}
